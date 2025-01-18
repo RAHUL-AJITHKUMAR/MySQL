2b5a8821e94335958c8ebf93f19e1ad921a54f90
RAHUL-AJITHKUMAR <rahul.ajithkumar5@gmail.com>
2025-01-19 00:26:38 +0530

    Update ASSIGNMENT 6.sql

--- ASSIGNMENT 6.sql
+++ ASSIGNMENT 6.sql
@@ -1,6 +1,6 @@
-create database Country_Population1;
+create database Country_Population;
 
-use Country_Population1;
+use Country_Population;
 
 create table Country(
 Country_Id int primary key,
@@ -11,9 +11,9 @@ Area int);
 desc country;
 
 create table Persons(
-Personal_Id int,
-Fname char(20) not null,
-Lname char(20),
+Personal_Id int primary key auto_increment,
+Fname varchar(20) not null,
+Lname varchar(20) not null,
 Population int,
 Rating decimal(5,2),
 Country_Id int,
@@ -34,6 +34,22 @@ Insert into Country values
 (95,'India',1095351995,3287590),
 (74,'Gambia',1641564,11300),
 (73,'Gabon',1424906,267667);
+
+select * from country;
+
+Insert into Persons values
+(1,'Michael','Scott',298444215,4.8,215,'United States'),
+(2,'Dwight','Schrute',298444215,4.6,215,'United States'),
+(3,'Jim','Halpert',60609153,4.0,214,'United Kingdom'),
+(4,'Pam','Beesly',33098932,4.7,37,'Canada'),
+(5,'Ryan','Howard',1424906,3.9,73,'Gabon'),
+(6,'Stanley','Hudson',1424906,4.8,73, NULL),
+(7,'Kevin','Malone',1062777,4.6,59,'East Timor'),
+(8,'Kelly','Kapoor',1095351995,3.8,95,'India'),
+(9,'Creed','Bratton',33098932,3.6,37,'Canada'),
+(10,'Toby','Flenderson',60609153,4.2,214,'United Kingdom'),
+(11,'Oscar','Martinez',25609153,4.3,200,NULL),
+(12,'Angela','Martin',2060915,3.2,214, NULL);
 
 select * from country;
 
